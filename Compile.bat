382e19614148f1ee18855efc178186fed2e2578e
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-01-16 00:59:27 +0200

    Fixed up Compile.bat MOAR GOTOS

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem Notepad++ is recommended to edit this file, or at least an IDE with syntax highlighting
 rem %1 is project to compile
 
 rem Remove "rem" from lines below to show all parameters when bat file launches
@@ -12,130 +13,108 @@ rem Setting GUI elements and going to the project directory
     title Compiling %~1...
     cd ..\%~1
 
-:msbuild
+:MSBuild
+    echo ==== Starting MSBuild compile for %~1 ====
+    "%WinDir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /property:Configuration=Release "%~1.sln"
 
-echo ==== Starting MSBuild compile for %~1 ====
-    rem Run the MSBuild command
-        "%WinDir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /property:Configuration=Release "%~1.sln"
-
-    rem If it doesn't fail, go to next step
-        if Not ERRORLEVEL==1 goto PortableCert
+if Not ERRORLEVEL==1 goto MSBuildDone
 
     echo MSBuild command failed, trying again in v3.5...
-        "%WinDir%\Microsoft.NET\Framework\v3.5\msbuild.exe" /property:Configuration=Release "%~1.sln"
+    "%WinDir%\Microsoft.NET\Framework\v3.5\msbuild.exe" /property:Configuration=Release "%~1.sln"
 
-        if Not ERRORLEVEL==1 goto PortableCert
+if Not ERRORLEVEL==1 goto MSBuildDone
 
     echo MSBuild_v3.5 command failed, trying again in v3.0...
-        "%WinDir%\Microsoft.NET\Framework\v3.0\msbuild.exe" /property:Configuration=Release "%~1.sln"
+    "%WinDir%\Microsoft.NET\Framework\v3.0\msbuild.exe" /property:Configuration=Release "%~1.sln"
 
-        if Not ERRORLEVEL==1 goto PortableCert
+if Not ERRORLEVEL==1 goto MSBuildDone
 
     color 0C
-        echo ==== MSBuild Commands Failed! ====
-        echo.
+    echo ==== MSBuild Commands Failed! ====
+    echo.
         echo Press enter to start installer...
         pause
         color %defaultColor%
-        %~dp0DotNetFramework4.6.exe
-    echo Installer launched, press enter to retry MSBuild commands...
+    %~dp0DotNetFramework4.6.exe
+        echo Installer launched, press enter to retry MSBuild commands...
         pause
-        goto msbuild
+        echo.
+        goto MSBuild
 
-:PortableCert
-
+:MSBuildDone
     echo ==== MSBuild Done ====
     echo.
+
+:PortableCert
     echo ==== Signing %~1.exe ====
         echo %~dp0WalkmanOSS.cer
         echo %~dp0WalkmanOSS.pvk
         echo https://github.com/Walkman100/%~1
         echo http://timestamp.verisign.com/scripts/timstamp.dll
     %~dp0signtool signwizard "%~dp0..\%~1\bin\Release\%~1.exe"
-        
+
 if Not ERRORLEVEL==1 goto PortableCertDone
-        
+
     echo ==== Signing %~1.exe Failed! ====
-        echo.
-        goto nsis
+    echo.
+        goto NSIS
 
 :PortableCertDone
     echo ==== Signing %~1.exe done ====
     echo.
-    
-:nsis
-    echo ==== Starting MakeNSIS Installer script for %~1 ====
-    rem Run the MakeNSIS command
-        "%ProgramFiles%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
-        echo ==== MakeNSIS Script done ====
-        echo.
 
-    rem If it doesn't fail, go to next step
-        if Not ERRORLEVEL==1 goto InstallerCert
+:NSIS
+    echo ==== Starting MakeNSIS Installer script for %~1 ====
+    "%ProgramFiles%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
+
+if Not ERRORLEVEL==1 goto NSISdone
 
     echo MakeNSIS command failed, trying again in 32-bit program files folder...
-        "%ProgramFiles(x86)%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
-        echo ==== MakeNSIS Script done ====
-        echo.
+    "%ProgramFiles(x86)%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
 
-        if Not ERRORLEVEL==1 goto InstallerCert
+if Not ERRORLEVEL==1 goto NSISdone
 
     echo MakeNSIS-32 command failed, trying again in 64-bit program files folder...
-        "%ProgramW6432%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
-        echo ==== MakeNSIS Script done ====
-        echo.
+    "%ProgramW6432%\NSIS\makensis.exe" "NSIS Installer for %~1.nsi"
 
-        if Not ERRORLEVEL==1 goto InstallerCert
+if Not ERRORLEVEL==1 goto NSISdone
 
     color 0C
-        echo ==== MakeNSIS Commands Failed ====
-        echo.
+    echo ==== MakeNSIS Commands Failed! ====
+    echo.
         echo Press enter to start installer...
         pause
         color %defaultColor%
-        %~dp0NSIS-2.46-setup.exe
-    echo Installer launched, press enter to retry MakeNSIS commands...
+    %~dp0NSIS-2.46-setup.exe
+        echo Installer launched, press enter to retry MakeNSIS commands...
         pause
-        goto nsis
+        echo.
+        goto NSIS
+
+:NSISdone
+    echo ==== MakeNSIS Script done ====
+    echo.
 
 :InstallerCert
-
-    rem because you can't use a PVK (private key) via command line -_-
     echo ==== Starting Certificate signing process ====
         echo %~dp0WalkmanOSS.cer
         echo %~dp0WalkmanOSS.pvk
         echo https://github.com/Walkman100/%~1
         echo http://timestamp.verisign.com/scripts/timstamp.dll
     %~dp0signtool signwizard "%~dp0..\%~1\bin\Release\%~1-Installer.exe"
-        echo ==== Certificate signing process done ====
-        echo.
-        
-        if Not ERRORLEVEL==1 goto rarCheck
-        
-        echo ==== Certificate signing Failed ====
-        echo.
-        
-    rem echo Certifying files...
-        rem echo Certifying installer...
-        rem "%ProgramFiles%\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /f %~dp0WalkmanOSS.cer /k %~dp0WalkmanOSS.pvk /du "https://github.com/Walkman100/%~1" /t "http://timestamp.verisign.com/scripts/timstamp.dll" "%~dp0..\%~1\bin\Release\%~1-Installer.exe"
-        rem if Not ERRORLEVEL==1 goto PortableCert
-        
-        rem echo Installer Certification failed!
 
-rem :PortableCert
+if Not ERRORLEVEL==1 goto InstallerCertDone
 
-        rem echo.
-        rem echo Certifying portable...
-        rem "%ProgramFiles%\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /f %~dp0WalkmanOSS.cer /k %~dp0WalkmanOSS.pvk /du "https://github.com/Walkman100/%~1" /t "http://timestamp.verisign.com/scripts/timstamp.dll" "%~dp0..\%~1\bin\Release\%~1.exe"
-        rem if Not ERRORLEVEL==1 goto rarCheck
-        
-        rem echo Portable Certification failed!
-        rem echo.
+    echo ==== Certificate signing Failed ====
+    echo.
+        goto rarCheck
+
+:InstallerCertDone
+    echo ==== Certificate signing process done ====
+    echo.
 
 :rarCheck
-
-        echo.
-        echo ==== Portable file operations ====
+    echo ==== Portable file operations ====
     rem Check if project must be rarred...
         if %~1==GitUpdater goto rar
         if %~1==BasicBrowserGecko goto rar
@@ -157,4 +136,5 @@ rem :PortableCert
         explorer.exe "bin\Release"
     @echo off
         timeout /t 5
+
 :EOF
