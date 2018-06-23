08b8138207a8acd182e91c8429e011e2a289ad2c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2018-06-23 17:25:02 +0200

    made zip scripts match main compile script

--- ZIP-GitUpdater.bat
+++ ZIP-GitUpdater.bat
@@ -1,15 +1,12 @@
 :zip
-
-echo ZIPping portable release with 7-Zip...
-rem Run the 7-Zip command
+    echo ==== ZIPping portable release with 7-Zip ====
     pushd bin\Release
     "%ProgramFiles%\7-Zip\7z.exe" a -tzip -- GitUpdater-Portable.zip GitUpdater.exe GitUpdater.bat OpenRepoInBash.bat OpenRepoInPS.bat PS
     popd
 
-rem If it doesn't fail, go to next step
 if Not ERRORLEVEL==1 goto done
 
-echo 7-Zip command failed, trying again in 32-bit program files folder...
+    echo 7-Zip command failed, trying again in 32-bit program files folder...
     pushd bin\Release
     "%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip -- GitUpdater-Portable.zip GitUpdater.exe GitUpdater.bat OpenRepoInBash.bat OpenRepoInPS.bat PS
     popd
@@ -18,25 +15,30 @@ if Not ERRORLEVEL==1 goto done
 
 echo 7-Zip_32 command failed, trying again in 64-bit program files folder...
     pushd bin\Release
-    "%ProgramW6432%\WinRAR\WinRAR.exe" a -tzip -- GitUpdater-Portable.zip GitUpdater.exe GitUpdater.bat OpenRepoInBash.bat OpenRepoInPS.bat PS
+    "%ProgramW6432%\7-Zip\7z.exe" a -tzip -- GitUpdater-Portable.zip GitUpdater.exe GitUpdater.bat OpenRepoInBash.bat OpenRepoInPS.bat PS
     popd
 
 if Not ERRORLEVEL==1 goto done
 
-color 0C
-echo 7-Zip ZIP Commands Failed!
-echo Press enter to start installer...
-pause
-color 0A
-%~dp07-Zip_v18.01_x64.exe
-echo Installer launched, press enter to retry 7-Zip commands...
-pause
-goto zip
+    color 0C
+    echo ==== 7-Zip ZIP Commands Failed! ====
+    echo.
+        echo Press enter to start installer...
+        pause
+        color 0A
+    %~dp07-Zip_v18.01_x64.exe
+        echo Installer launched, press enter to retry 7-Zip commands...
+        pause
+        echo.
+        goto zip
 
 :done
-echo ZIPping portable release with 7-Zip done.
-
-rem Since it doesn't seem to return control, just run the code:
-echo launching Explorer...
-explorer.exe "bin\Release"
-timeout /t 1
+    echo ==== ZIPping portable release with 7-Zip done ====
+    
+    rem Since it doesn't seem to return control, just run the code:
+    echo.
+    echo ==== Launching Explorer ====
+    @echo on
+        explorer.exe "bin\Release"
+    @echo off
+        timeout /t 5
